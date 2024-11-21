a9c4d128f48b81a4cb586aeea403c6ce95850359
João Pedro Mello <153293516+joaomello7@users.noreply.github.com>
2024-11-20 21:42:14 -0300

    Conexao com nuvem

---
+++ config/conexao.php
@@ -0,0 +1,23 @@
+<?php
+require '../vendor/autoload.php';
+
+try {
+    // Conectar ao MongoDB Atlas
+    $client = new MongoDB\Client("mongodb+srv://user:@cluster0.du21r.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0");
+
+    // Selecionar o banco de dados
+    $db = $client->selectDatabase('Trabalho_Mongo');
+
+    // Testar a conexão listando as coleções
+    $collections = $db->listCollections();
+    
+    echo "Conexão bem-sucedida! As coleções no banco de dados são:<br>";
+    
+    foreach ($collections as $collection) {
+        echo $collection->getName() . "<br>";
+    }
+} catch (Exception $e) {
+    // Exibir erro se houver problemas na conexão
+    echo "Erro ao conectar ao MongoDB: " . $e->getMessage();
+}
+?>
